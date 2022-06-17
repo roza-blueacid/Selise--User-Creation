2fd500726388f605bec8eeea9792fb68a345153a
Nishat Tasnim Roza <roza@Nishats-MacBook-Pro.local>
2022-06-17 21:56:55 +0600

    First Commit-Chechecking Netlify

---
+++ form-validation.js
@@ -0,0 +1,74 @@
+const form = document.getElementById('form');
+const firstName = document.getElementById('firstname');
+const lastName = document.getElementById('lastname');
+const gender = document.getElementById('gender');
+
+
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+const password2 = document.getElementById('password2');
+
+form.addEventListener('submit', e => {
+	e.preventDefault();
+	
+	checkInputs();
+});
+
+function checkInputs() {
+	// trim to remove the whitespaces
+	const firstNameValue = firstName.value.trim();
+    const lastNameValue = lastName.value.trim();
+	const emailValue = email.value.trim();
+	const passwordValue = password.value.trim();
+	const password2Value = password2.value.trim();
+	
+	if(firstNameValue === '') {
+		setErrorFor(firstName, 'First Name cannot be blank');
+	} else {
+		setSuccessFor(firstName);
+	}
+    if(lastNameValue === '') {
+		setErrorFor(lasttName, 'Last Name cannot be blank');
+	} else {
+		setSuccessFor(lasttName);
+	}
+	
+	if(emailValue === '') {
+		setErrorFor(email, 'Email cannot be blank');
+	} else if (!isEmail(emailValue)) {
+		setErrorFor(email, 'Not a valid email');
+	} else {
+		setSuccessFor(email);
+	}
+	
+	if(passwordValue === '') {
+		setErrorFor(password, 'Password cannot be blank');
+	} else {
+		setSuccessFor(password);
+	}
+	
+	if(password2Value === '') {
+		setErrorFor(password2, 'Password2 cannot be blank');
+	} else if(passwordValue !== password2Value) {
+		setErrorFor(password2, 'Passwords does not match');
+	} else{
+		setSuccessFor(password2);
+	}
+}
+
+function setErrorFor(input, message) {
+	const formControl = input.parentElement;
+	const small = formControl.querySelector('small');
+	formControl.className = 'form-control error';
+	small.innerText = message;
+}
+
+function setSuccessFor(input) {
+	const formControl = input.parentElement;
+	formControl.className = 'form-control success';
+}
+	
+function isEmail(email) {
+	return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
+}
+
